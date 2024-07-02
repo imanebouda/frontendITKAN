057de3ac03c8dd928768408921b0f58ad649f548
AhmedLAZAAR <lazaarahmed87@gmail.com>
2024-07-02 01:22:43 +0100

    Ajouter les fonctionnalités de constat

--- src/app/bo/components/question/list-check-list/list-check-list.component.ts
+++ src/app/bo/components/question/list-check-list/list-check-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { QuestionModel } from 'src/app/models/question.model';
 import { QuestionService } from 'src/app/services/AuditServices/question.service';
 import { Modal } from 'bootstrap';
 import {HttpClient} from "@angular/common/http";
-
+import { ConstatModel } from 'src/app/models/constat.model';
+import { ConstatService } from 'src/app/services/AuditServices/constat.service';
+import { TypeContat } from 'src/app/models/type-contat.model';
 @Component({
   selector: 'app-list-check-list',
   templateUrl: './list-check-list.component.html',
@@ -27,13 +29,14 @@ export class ListCheckListComponent implements OnInit {
   is_loading: boolean = true;
   formulaireRecherche: FormGroup;
   typeQuestion: { label: string; value: number }[] = [];
+  typeConstats: TypeContat[] = [];
   selectedCheckList: QuestionModel;
   addChecklist : QuestionModel;
-
+  constatForm: FormGroup;
   @ViewChild('updateModal') updateModal: ElementRef;
   @ViewChild('addModal') addModal: ElementRef;
-
-  constructor(private questionService: QuestionService,private http: HttpClient) {}
+  @ViewChild('constatModal') constatModal: ElementRef;
+  constructor(private questionService: QuestionService, private constatService: ConstatService, private http: HttpClient) { }
   ngOnInit(): void {
     this.initializeForm();
     this.loadquestions();
@@ -45,6 +48,10 @@ export class ListCheckListComponent implements OnInit {
   initializeForm(): void {
     this.formulaireRecherche = new FormGroup({
       typeChecklist: new FormControl('')
+    });
+    this.constatForm = new FormGroup({
+      constat: new FormControl('', Validators.required),
+      typeConstatId: new FormControl('', Validators.required)
     });
   }
 
@@ -128,5 +135,46 @@ export class ListCheckListComponent implements OnInit {
   closeAddDialog(): void {
     const modal = Modal.getInstance(this.addModal.nativeElement);
     modal.hide();
+  }
+  openAffectConstatModal(checkList: QuestionModel): void {
+    console.log('Selected Question:', checkList);
+    const modal = new Modal(this.constatModal.nativeElement);
+    modal.show();
+  }
+
+  saveConstat(): void {
+    if (this.constatForm.valid) {
+      const newConstat: ConstatModel = {
+        id: 0, // ou la valeur appropriée
+        constat: this.constatForm.get('constat')?.value,
+        typeConstatId: this.constatForm.get('typeConstatId')?.value,
+        questionId: this.selectedCheckList.id,
+        typeConstat: null,
+        question: null
+      };
+
+      this.constatService.createConstat(newConstat).subscribe(
+        response => {
+          console.log('Constat created successfully', response);
+          // Ajoutez une logique pour actualiser les données si nécessaire
+          this.closeAffectConstatModal();
+        },
+        error => {
+          console.error('Error creating Constat', error);
+        }
+      );
+    }
+  }
+
+  closeAffectConstatModal(): void {
+    if (this.constatModal && this.constatModal.nativeElement) {
+      const modal = Modal.getInstance(this.constatModal.nativeElement);
+      if (modal) {
+        modal.hide();
+      } else {
+        console.error('constatModal instance is not defined');
+      }
+      this.selectedCheckList = null;
+    }
   }
 }
